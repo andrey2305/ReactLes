8ece387d09f91df70b0eb53a2af262cfa92741a5
Andrew <akuzmichov.work@gmail.com>
2018-07-08 12:10:21 +0300

    first part slider

--- les2/src/components/Main.js
+++ les2/src/components/Main.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Slider from './Slider';
 
 const Repair = styled.div`
     color: #fff;
@@ -57,7 +58,7 @@ class Main extends Component {
                     </CallBtn>
                 </Col>
                 <Col lg={6} lgOffset={1}>
-
+                    <Slider />
                 </Col>
             </Row>
         )
